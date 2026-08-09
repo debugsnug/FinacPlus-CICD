2451143fb3f2b1b125dcff201c3798aeeb4de459
debugsnug <kiran.kishore.4414@gmail.com>
2026-08-09 22:21:12 +0530

    Update application version to v2

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.json({
     message: 'Hello from the CI/CD demo app!',
-    version: process.env.APP_VERSION || 'dev',
+    version: process.env.APP_VERSION || 'v2',
     hostname: require('os').hostname()
   });
 });
